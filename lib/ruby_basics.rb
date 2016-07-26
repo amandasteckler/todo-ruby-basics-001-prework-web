9cd7e6540df84a8c16e89188ce234a193c9fdf69
Amanda Steckler <amandajsteckler@gmail.com>
2016-07-26 01:36:34 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,32 @@
 def division(num1, num2)
- 
+ sum = num1/num2
+ sum
 end
 
 def assign_variable(value)
-  
+  value_of_argument = value
+  value_of_argument
 end
 
-def argue
-  
+def argue(phrase = "I'm right and you are wrong!")
+  phrase
 end
 
-def greeting
-
+def greeting(greeting, name)
+  greeting + name
 end
 
 def return_a_value
-
+  value = "Nice"
+  value
 end
 
 def last_evaluated_value
-
+  name = "Amanda" # just to add another value to method that's not equal to "expert"
+  phrase = "expert"
+  phrase
 end
 
-def pizza_party
-
+def pizza_party(topping = "cheese")
+  topping
 end
